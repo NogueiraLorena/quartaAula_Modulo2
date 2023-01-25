77d4550bbb1a0f99836e48f2e96a44b06586fe14
NogueiraLorena <bragalorena72@gmail.com>
2023-01-25 15:34:12 -0300

    contem pouca coisa, foco foi entender os laços de repetição

---
+++ script.js
@@ -0,0 +1,90 @@
+// ik = true;
+
+// switch () {
+//     case true:
+//         console.log("Usuário logado");
+//         break;
+//     case false:
+//         console.log("Usuário não autenticado");
+// }
+
+// Valor impresso no console: "Bem-vinda!"
+// perfume = "amadeirado";
+
+// switch () {
+//     case "feminino":
+//         console.log("");
+//         break;
+//     case false:
+//         console.log("Bem-vindo!");
+// }
+
+// Valor impresso no console: "Outono"
+//mes = "Maio";
+
+// switch (mes) {
+//     case "Janeiro":
+//     case "Fevereiro":
+//     case "Março":
+//         console.log("Verão!");
+//         break;//
+
+
+
+
+
+// var revenda = "perfume floral"
+
+// switch (revenda){
+// case "perfume floral":
+// console.log("Compra efetuada com sucesso")
+// break;
+
+
+// case "perfume amadeirados":
+// console.log("Tem certeza que não prefere outro aroma?")
+// break;
+
+
+// case "perfume ":
+// console.log("Tem certeza que não prefere outro perfume?")
+// default:
+
+
+// case "perfume frutal":
+// console.log("Tem certeza que não prefere outro aroma?")
+// break;
+
+
+// case "perfume":
+// console.log("Não trabalhamos com este tipo de perfume aqui")
+
+
+
+let revenda = "perfume cítrico"
+
+switch (revenda){
+case "perfume floral":
+console.log("Compra efetuada com sucesso")
+break;
+
+
+case "perfume amadeirados":
+console.log("Tem certeza que não prefere outro aroma?")
+break;
+
+
+case "perfume cítrico":
+console.log("Tem certeza que não prefere outro aroma?")
+break;
+
+
+case "perfume frutal":
+console.log("Tem certeza que não prefere outro aroma?")
+break;
+
+
+case "perfume":
+console.log("Não trabalhamos com este tipo de perfume aqui")
+default:
+}
